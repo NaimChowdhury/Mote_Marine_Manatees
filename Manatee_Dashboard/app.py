6b00b7495032855b9b884c9afdb5b83ee64981e8
natewagner10 <54585457+natewagner10@users.noreply.github.com>
2020-07-14 15:11:16 -0400

    Update app.py

--- Manatee_Dashboard/app.py
+++ Manatee_Dashboard/app.py
@@ -27,45 +27,6 @@ import os
 import json
 import numpy as np
 
-#img = io.imread('/Users/natewagner/Documents/Mote_Manatee_Project/data/MMLDUs_BatchA/U3983_A.jpg')
-#fig = px.imshow(img)
-#fig.show()
-
-
-
-image_filename = '/Users/natewagner/Documents/Mote_Manatee_Project/data/MMLDUs_BatchA/U3983_A.jpg' # replace with your own image
-encoded_image = base64.b64encode(open(image_filename, 'rb').read())
-
-
-
-
-image_filename2 = '/Users/natewagner/Documents/Mote_Manatee_Project/data/MMLDUs_BatchA/U3983_A.jpg' # replace with your own image
-encoded_image2 = base64.b64encode(open(image_filename2, 'rb').read())
-
-#image_filename3 = '/Users/natewagner/Documents/Mote_Manatee_Project/data/MMLDUs_BatchA/U3983_B.jpg' # replace with your own image
-#encoded_image3 = base64.b64encode(open(image_filename2, 'rb').read())
-
-######################################################################################################
-
-path = '/Users/natewagner/Documents/Mote_Manatee_Project/data/MMLDUs_BatchA/' 
-
-
-flist = list()
-for file in os.listdir(path):
-    data = dict()
-    base = os.path.basename(path + file)
-    data["label"] = base
-    open_file = open(path + file,'rb')
-    image_read = open_file.read()
-    image_64_encode = base64.encodebytes(image_read)
-    data["data"] = image_64_encode.decode('ascii')
-    flist.append(data)     
-
-    final_data = json.dumps({'files':flist}, sort_keys=True, indent=4, separators=(',', ': '))
-    #return render_template("images.html", final_data=final_data)
-
-
-final_data[1]
 
 
 ######################################################################################################
@@ -95,12 +56,9 @@ encoding_string = base64.b64encode(im_bytes).decode("ascii")
 
 
 
-
-
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(external_stylesheets=[dbc.themes.YETI])
 
    
-    
     
     
 
@@ -124,7 +82,7 @@ app.layout = html.Div(
                                 ]),
                                 style={
                                     'width': '80%',
-                                    'height': '200px',
+                                    'height': '100px',
                                     'lineHeight': '60px',
                                     'borderWidth': '1px',
                                     'borderStyle': 'dashed',
@@ -133,7 +91,6 @@ app.layout = html.Div(
                                     'margin': '10px',
                                     "margin-left": "28px",
                                 },
-                                # Allow multiple files to be uploaded
                                 multiple=True
                             ),], style={"width": "18rem", 
                                          "margin-left": "50px",
@@ -143,31 +100,31 @@ app.layout = html.Div(
                 dbc.Col(
                     dbc.Card([
                         dbc.CardHeader(
-                            html.H1("Sketch", className="card-text")),
+                            html.H1("Sketch", className="card-text"), style={"width": "25rem", 'textAlign': 'center'}),
                                 html.Div(id='output-image-upload', style = {'textAlign': 'center',
-                                                                            'width': '80%',
-                                                                            'height': '615px',
+                                                                            'height': '680px',
                                                                             'lineHeight': '60px',
-                                                                            #'borderWidth': '5px',
-                                                                            #'borderStyle': 'solid',
-                                                                            #'borderRadius': '5px',
-                                                                            'margin-left': '40px'}), dbc.CardFooter(" ")]), width = 3),               
+                                                                            'align-items': 'center'}), dbc.CardFooter(" ", style = {"width": "25rem"})], style = {'align-items': 'center', 
+                                                                                                                                                                  'width': '25rem'}), width = 3),               
                 dbc.Col(
                     dbc.Card([
                         dbc.CardHeader(
-                            html.H1("Browse Matches", className="card-text")),
+                            html.H1("Browse Matches", className="card-text", style = {'textAlign': 'center'}), style={"width": "50rem"}),
                                 html.Div([
                                     dbc.Row(
                                         [
-                                            dbc.Col(dbc.Button("Left", id="left_click", color="primary", className="mr-2"), style = {'textAlign': 'left'}),
-                                            dbc.Col(html.Span(id="example-output2", style={"vertical-align": "middle"})),
-                                            dbc.Col(dbc.Button("Right", id="right_click", color="primary", className="mr-2"), style = {'textAlign': 'right'}),
+                                            dbc.Col(dbc.Row([dbc.Button("Left", id="left_click", color="primary", className="mr-2", n_clicks = 0, size="lg")]), style = {'Align': 'left', 'vertical-align': 'middle'}, width = 2),
+                                            dbc.Col(html.Span(id="example-output2", style={"vertical-align": "middle"}), width = 8),
+                                            dbc.Col(dbc.Row([dbc.Button("Right", id="right_click", color="primary", className="mr-2", n_clicks = 0, size="lg")]), style = {'Align': 'right', 'vertical-align': 'middle'}, width = 2),
                                         ]),], style = {'textAlign': 'center',
                                                        'width': '80%',
-                                                       'height': '550px',
-                                                       'margin': '40px',}),
-                                                dbc.CardFooter("Probability: 98%, Matches: 35"),
-                                ], style={"width": "50rem"}),  width = 6),
+                                                       'height': '600px',
+                                                       'margin': '40px',
+                                                       'align-items': 'center',
+                                                       'margin-top': '20px'}),
+                                                dbc.CardFooter("Probability: 98%, Matches: 35", style={"width": "50rem"}),
+                                ], style={"width": "50rem",
+                                          "align-items": "center"}),  width = 6),
                             ]
                         ),
                     ]
@@ -183,26 +140,10 @@ def parse_contents(contents, filename, date):
     string = contents.split(";base64,")[-1]
     im_pil = drc.b64_to_pil(string)
     im_pil = im_pil.filter(ImageFilter.EDGE_ENHANCE_MORE)
-    #buff = _BytesIO()
-    #im_pil.save(buff, format="png")
-    #im_bytes = im_pil.tobytes()
-    #encoding_string = base64.b64encode(buff.getvalue()).decode("utf-8")
-    #encoding_string = drc.pil_to_b64(im_pil)
     return html.Div([
+        html.Br(),
         html.H5("Manatee ID:     " + filename[0:7]),
-        #html.H6(datetime.datetime.fromtimestamp(date)),
-
-        # HTML images accept base64 encoded strings in the same format
-        # that is supplied by the upload
-        #drc.InteractiveImagePIL(
-        #    image_id="interactive-image",
-        #    image=im_pil,
-            #enc_format=enc_format,
-            #dragmode=dragmode,
-            #verbose=DEBUG,
-        #),
         html.Img(src=contents),
-        #html.Hr(),
     ])
 
 
@@ -230,37 +171,45 @@ def return_image(n):
     image_filename2 = '/Users/natewagner/Documents/Mote_Manatee_Project/data/MMLDUs_BatchA/'
     global names
     file = names[n]
+    
     encoded_image = base64.b64encode(open(image_filename2 + file, 'rb').read())
-    return html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()))
+    return html.Div([
+        html.H5("Manatee ID: " + names[n][0:7]),
+        html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()))
+        ], style = {'align-items': 'center'})
 
 
 
 
 
+count = 0
+count2 = 0
+switch = False
 
 @app.callback(
-    Output("example-output2", "children"), [Input("right_click", "n_clicks")]
+    Output("example-output2", "children"),
+    [
+     Input("right_click", "n_clicks"),
+     Input("left_click", "n_clicks")
+     ]
 )
-def on_button_click(n):
-    print(n)
-    if n is None:
+def on_button_click(n, n2):
+    global count
+    global switch
+    global count2
+    print("N is ", n2, "N2 is ", n)
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    print(changed_id)
+    if "right_click" in changed_id:
+        count += 1
+        return return_image(count)
+    if "left_click" in changed_id:
+        count -= 1
+        return return_image(count)
+    else:
         return html.Img(src='data:image/png;base64,{}'.format(blank.decode()))
-    else:
-        return return_image(n)
-
-
-@app.callback(
-    Output("example-output", "children"), [Input("left_click", "n_clicks")]
-)
-def on_button_click2(n):
-    global clicks
-    if n is None:
-        return " "
-    else:
-        return html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()))
-
-
-
+    
+    
 
 
 
