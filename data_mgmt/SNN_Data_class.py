edea35a2b51b09afba6bf97f72ee15b9bc7c4a47
rosagradilla19 <54587899+rosagradilla19@users.noreply.github.com>
2020-07-17 10:56:55 -0400

    Add files via upload
    Class to label data

---
+++ data_mgmt/SNN_Data_class.py
@@ -0,0 +1,66 @@
+import os
+import cv2
+import numpy as np
+
+""" 
+To use this class:
+initiate a class instance
+siamese = SiameseNetworkDataset(data_folder, IMSIZE)
+siamese.make_data('out_name')
+
+The Data can be loaded from any other file by loading it through numpy:
+np.load('out.npy', allow_pickle=True)
+"""
+
+class SiameseNetworkDataset(object):
+    """ 
+    This class outputs a .npy file with the images and label as (img0,img1,label)
+    it takes dataset path and image size as arguments
+    """
+    def __init__(self, PATH, IMG_SIZE):
+        self.path = PATH
+        self.img_size = IMG_SIZE
+    
+    LABELS = {'similar': 1, 'not_similar':0}
+    
+    training_data = []
+    
+    def make_data(self, out_name):
+        # provide a name for .npy file being written
+        i = 0
+        file_names = []
+
+        # First get all file names in list file_names
+        for file in sorted(os.listdir(self.path)):
+            file_names.append(str(file))
+
+        for i in range(len(file_names)): 
+            # Check if they are pairs
+            if file_names[i][-5] == 'A' and file_names[i+1][-5] == 'B':
+                img0 = os.path.join(self.path, file_names[i])
+                img1 = os.path.join(self.path, file_names[i+1])
+                img2 = os.path.join(self.path, file_names[np.random.randint(0, len(file_names))])
+                img3 = os.path.join(self.path, file_names[np.random.randint(0, len(file_names))])
+
+                # Read the image
+                img0 = cv2.imread(img0, cv2.IMREAD_UNCHANGED)
+                img1 = cv2.imread(img1, cv2.IMREAD_UNCHANGED)
+                img2 = cv2.imread(img2, cv2.IMREAD_UNCHANGED)
+                img3 = cv2.imread(img3, cv2.IMREAD_UNCHANGED)
+
+                # Resize according to IMG_SIZE
+                img0 = cv2.resize(img0, (self.img_size, self.img_size), interpolation= cv2.INTER_AREA)
+                img1 = cv2.resize(img1, (self.img_size, self.img_size), interpolation= cv2.INTER_AREA)
+                img2 = cv2.resize(img2, (self.img_size, self.img_size), interpolation= cv2.INTER_AREA)
+                img3 = cv2.resize(img3, (self.img_size, self.img_size), interpolation= cv2.INTER_AREA)
+
+                # append to training_data
+                self.training_data.append([np.array(img0), np.array(img1), np.eye(2)[self.LABELS['similar'] ]])
+                self.training_data.append([np.array(img0), np.array(img2), np.eye(2)[self.LABELS['not_similar'] ]])
+                self.training_data.append([np.array(img0), np.array(img3), np.eye(2)[self.LABELS['not_similar'] ]])
+                
+            # else, continue
+            i += 1
+        
+        np.save(out_name + '.npy', self.training_data )
+
